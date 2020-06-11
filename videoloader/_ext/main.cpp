8c5b01d07ca385e5b018555b8e11c0ec1a92b30f
胡玮文 <huww98@outlook.com>
2020-06-12 01:01:44 +0800

    test main recursive support

--- videoloader/_ext/main.cpp
+++ videoloader/_ext/main.cpp
@@ -30,12 +30,18 @@ int main(int argc, char const *argv[]) {
     std::filesystem::path base = "/tmp/answering_questions";
     VideoLoader loader;
     vector<Video> videos;
-    for (auto &f : std::filesystem::directory_iterator(base)) {
+    for (auto &f : std::filesystem::recursive_directory_iterator(base)) {
+        if (f.path().extension() != ".mp4") {
+            continue;
+        }
         // cout << f.path() << endl;
         auto video = loader.addVideoFile(f.path());
         video.sleep();
         // video.getBatch({14, 15});
         videos.push_back(move(video));
+        if (videos.size() % 1000 == 0) {
+            spdlog::info("{} videos opend...", videos.size());
+        }
     }
 
     spdlog::info("{} videos opened.", videos.size());
