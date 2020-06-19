02380d96771597f55557ca27da30a61193ba12e5
胡玮文 <huww98@outlook.com>
2020-06-19 22:58:23 +0800

    more tar tests

--- videoloader/_ext/tests/tar_iterator_tests.cpp
+++ videoloader/_ext/tests/tar_iterator_tests.cpp
@@ -15,12 +15,16 @@ TEST(TarIterator, Iterate) {
     std::deque<expected_tar_entry> expected_entries = {
         {"testtar/", huww::tar_entry_type::directory, 512, 0},
         {"testtar/testfile.txt", huww::tar_entry_type::file, 1024, 14},
-        {"testtar/testfile1.txt", huww::tar_entry_type::file, 2048, 14},
+        {"testtar/zero-length-file.txt", huww::tar_entry_type::file, 2048, 0},
+        {"testtar/testfile1.txt", huww::tar_entry_type::file, 2560, 14},
+        {"testtar/This-is-a-file-with-path-name-length-equals-to-100-bytes-loooooooooooooooooooong"
+         "-exactly.txt",
+         huww::tar_entry_type::file, 3584, 11},
         {"testtar/This-is-a-file-with-very-"
          "loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo"
          "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo"
          "oooooong-file-name.txt",
-         huww::tar_entry_type::file, 4096, 13},
+         huww::tar_entry_type::file, 5632, 13},
     };
     for (auto &entry : huww::tar_iterator("./tests/tar/test.tar")) {
         EXPECT_GT(expected_entries.size(), 0);
@@ -32,4 +36,10 @@ TEST(TarIterator, Iterate) {
         expected_entries.pop_front();
     }
     EXPECT_EQ(expected_entries.size(), 0);
+}
+
+TEST(TarIterator, IterateBreak) {
+    for ([[maybe_unused]]auto &entry : huww::tar_iterator("./tests/tar/test.tar")) {
+        break; // Should not have memory leak.
+    }
 }
